2804aa09cfebd1790e246e55fe7a453cc5a760b1
ladp17 <leonardoadpereira@gmail.com>
2019-07-23 22:27:27 -0300

    msm coisa, bugsinho resolvido

--- src/main/java/Palavra.java
+++ src/main/java/Palavra.java
@@ -29,15 +29,15 @@ public class Palavra {
 
     public int checkLeter(char letra){
 
-        int v; //vetor q guarda os indices onde foi encontrada a letra, mas num unico int. suponha q foi encontrada no i=0 e i=4 entao v = 40
+        int v; //vetor q guarda os indices onde foi encontrada a letra, mas num unico int. suponha q foi encontrada no i=0 e i=4 entao v = 400. tem um 0 de guarda como primeiro digito para evitar problemas
         int e;
 
-        e = 0;
+        e = 1;
         v = 0;
 
         for (int i = 0; i < this.getTam(); i++) {
 
-            if (this.palavra.charAt(i) == letra) { 
+            if (this.palavra.charAt(i) == letra) {
                 v += i * Math.pow(10, e);
                 e++;
             }
