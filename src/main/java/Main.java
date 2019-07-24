71509c1ed471ccaa1740b7cc4d146d1fc3d3a52c
ladp17 <leonardoadpereira@gmail.com>
2019-07-23 23:18:06 -0300

    classe Letra supostamente concluida

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,27 @@ public class Main {
     public static void main(String[] args) {
 
         Palavra palavra = new Palavra();
+        Letra letra = new Letra();
+
         palavra.setPalavra();
 
+
+
+        //mais debug
+//        while (true) {
+//
+//            letra.setLetra();
+//
+//            letra.printJaTentadas();
+//        }
 
 
 
 
         // Debug
-        palavra.checkLeter('t');
-        System.out.println(palavra.getPalavra());
-        System.out.println(palavra.getTam());
+//        palavra.checkLeter(letra.getLetra());
+//        System.out.println(palavra.getPalavra());
+//        System.out.println(palavra.getTam());
 
 
     }
